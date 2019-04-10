6ad56b1744216807e6fb12052d032b77916247c4
Kenneth Jaysone Francis <kennethjaysone@gmail.com>
2019-04-11 01:21:03 +0800

    added semantic-ui-css and semantic-ui-react. Added dashboard skeleton and also navbar

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import NavBar from "../features/nav/navBar/NavBar";
+import EventDashboard from "../features/event/eventdashboard/EventDashboard";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Revents</h1>
-      </div>
+      <React.Fragment>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </React.Fragment>
     );
   }
 }
